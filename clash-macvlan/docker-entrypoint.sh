e4b7871a26b2d8323b6bc3196f10c5f18afe5469
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-11-03 02:48:23 +0800

    Update docker-entrypoint.sh

--- clash-macvlan/docker-entrypoint.sh
+++ clash-macvlan/docker-entrypoint.sh
@@ -2,18 +2,49 @@
 ip rule add fwmark 1 lookup 1
 ip route add local default dev lo table 1
 
-iptables -t nat -A PREROUTING -d $SUBNET -j RETURN
-iptables -t nat -A PREROUTING -m addrtype ! --dst-type UNICAST -j RETURN
+iptables -t nat -A PREROUTING -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A PREROUTING -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A PREROUTING -d 100.64.0.0/10 -j RETURN
+iptables -t nat -A PREROUTING -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A PREROUTING -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A PREROUTING -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A PREROUTING -d 192.0.0.0/24 -j RETURN
+iptables -t nat -A PREROUTING -d 192.0.2.0/24 -j RETURN
+iptables -t nat -A PREROUTING -d 192.88.99.0/24 -j RETURN
+iptables -t nat -A PREROUTING -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A PREROUTING -d 198.51.100.0/24 -j RETURN
+iptables -t nat -A PREROUTING -d 203.0.113.0/24 -j RETURN
+iptables -t nat -A PREROUTING -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A PREROUTING -d 233.252.0.0/24 -j RETURN
+iptables -t nat -A PREROUTING -d 240.0.0.0/4 -j RETURN
+iptables -t nat -A PREROUTING -d 255.255.255.255/32 -j RETURN
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 53 -j REDIRECT --to-ports 53
 iptables -t nat -A PREROUTING -p udp -m udp --dport 53 -j REDIRECT --to-ports 53
 
 iptables -t nat -A POSTROUTING -o lo -j RETURN
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
-iptables -t mangle -A PREROUTING -m addrtype ! --dst-type UNICAST -j RETURN
+iptables -t mangle -A PREROUTING -d 0.0.0.0/8 -j RETURN
+iptables -t mangle -A PREROUTING -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A PREROUTING -d 100.64.0.0/10 -j RETURN
+iptables -t mangle -A PREROUTING -d 127.0.0.0/8 -j RETURN
+iptables -t mangle -A PREROUTING -d 169.254.0.0/16 -j RETURN
+iptables -t mangle -A PREROUTING -d 172.16.0.0/12 -j RETURN
+iptables -t mangle -A PREROUTING -d 192.0.0.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -d 192.0.2.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -d 192.88.99.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -d 192.168.0.0/16 -j RETURN
+iptables -t mangle -A PREROUTING -d 198.51.100.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -d 203.0.113.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -d 224.0.0.0/4 -j RETURN
+iptables -t mangle -A PREROUTING -d 233.252.0.0/24 -j RETURN
+iptables -t mangle -A PREROUTING -d 240.0.0.0/4 -j RETURN
+iptables -t mangle -A PREROUTING -d 255.255.255.255/32 -j RETURN
 iptables -t mangle -A PREROUTING -p udp -m udp --dport 443 -j DROP
 iptables -t mangle -A PREROUTING -p tcp -m tcp --dport 53 -j RETURN
 iptables -t mangle -A PREROUTING -p udp -m udp --dport 53 -j RETURN
+iptables -t mangle -A PREROUTING ! -d 198.18.0.0/15 -p tcp -j RETURN
+iptables -t mangle -A PREROUTING ! -d 198.18.0.0/15 -p udp -j RETURN
 iptables -t mangle -A PREROUTING -p tcp -j TPROXY --on-port 2001 --on-ip 0.0.0.0 --tproxy-mark 1
 iptables -t mangle -A PREROUTING -p udp -j TPROXY --on-port 2001 --on-ip 0.0.0.0 --tproxy-mark 1
 
