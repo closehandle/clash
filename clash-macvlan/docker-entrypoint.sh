d5260fc695c8f4fe519bff406d18277a0c05aece
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-11-02 19:20:13 +0000

    Add files via upload

--- clash-macvlan/docker-entrypoint.sh
+++ clash-macvlan/docker-entrypoint.sh
@@ -55,7 +55,7 @@ while read -r i; do
     cfgs="/etc/clash/$name.yml"
     dirs="/var/lib/clash/$name"
     [[ ! -d "$dirs" ]] && mkdir "$dirs"
-    [[ ! -f "$dirs/Country.mmdb" ]] && cp -f /var/lib/clash/Country.mmdb "$dirs"
+    [[ ! -f "$dirs/Country.mmdb" ]] && cp -f "$GEOIP" "$dirs"
 
     clash -d "$dirs" -f "$cfgs" &
 done < <(ls -1 /etc/clash)
--- clash/docker-entrypoint.sh
+++ clash/docker-entrypoint.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+GEOIP="/var/lib/clash/Country.mmdb"
 
 while read -r i; do
     name=$(echo $i | sed 's|.yml||g')
     cfgs="/etc/clash/$name.yml"
     dirs="/var/lib/clash/$name"
     [[ ! -d "$dirs" ]] && mkdir "$dirs"
-    [[ ! -f "$dirs/Country.mmdb" ]] && cp -f /var/lib/clash/Country.mmdb "$dirs"
+    [[ ! -f "$dirs/Country.mmdb" ]] && cp -f "$GEOIP" "$dirs"
 
     clash -d "$dirs" -f "$cfgs" &
 done < <(ls -1 /etc/clash)
