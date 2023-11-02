42c1383357426ab6625bfeeae2d6f40475faf243
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-11-02 19:14:48 +0000

    Add files via upload

--- clash-macvlan/docker-entrypoint.sh
+++ clash-macvlan/docker-entrypoint.sh
@@ -52,23 +52,13 @@ iptables -t mangle -A PREROUTING -p udp -j TPROXY --on-port 2001 --on-ip 0.0.0.0
 
 while read -r i; do
     name=$(echo $i | sed 's|.yml||g')
+    cfgs="/etc/clash/$name.yml"
+    dirs="/var/lib/clash/$name"
+    [[ ! -d "$dirs" ]] && mkdir "$dirs"
+    [[ ! -f "$dirs/Country.mmdb" ]] && cp -f /var/lib/clash/Country.mmdb "$dirs"
 
-    if [[ "$name" != 'default' ]]; then
-        cfgs="/etc/clash/$name.yml"
-        dirs="/var/lib/clash/$name"
-
-        if [[ ! -d "$dirs" ]]; then
-            mkdir  "$dirs"
-            cp -f  /var/lib/clash/Country.mmdb "$dirs"
-        fi
-
-        clash -d "$dirs" -f "$cfgs" &
-    fi
+    clash -d "$dirs" -f "$cfgs" &
 done < <(ls -1 /etc/clash)
 
-if [[ ! -d /var/lib/clash/default ]]; then
-    mkdir  /var/lib/clash/default
-    cp -f  /var/lib/clash/Country.mmdb /var/lib/clash/default
-fi
-
-exec clash -d /var/lib/clash/default -f /etc/clash/default.yml
+wait -n
+exit $?
--- clash/docker-entrypoint.sh
+++ clash/docker-entrypoint.sh
@@ -2,23 +2,13 @@
 
 while read -r i; do
     name=$(echo $i | sed 's|.yml||g')
+    cfgs="/etc/clash/$name.yml"
+    dirs="/var/lib/clash/$name"
+    [[ ! -d "$dirs" ]] && mkdir "$dirs"
+    [[ ! -f "$dirs/Country.mmdb" ]] && cp -f /var/lib/clash/Country.mmdb "$dirs"
 
-    if [[ "$name" != 'default' ]]; then
-        cfgs="/etc/clash/$name.yml"
-        dirs="/var/lib/clash/$name"
-
-        if [[ ! -d "$dirs" ]]; then
-            mkdir  "$dirs"
-            cp -f  /var/lib/clash/Country.mmdb "$dirs"
-        fi
-
-        clash -d "$dirs" -f "$cfgs" &
-    fi
+    clash -d "$dirs" -f "$cfgs" &
 done < <(ls -1 /etc/clash)
 
-if [[ ! -d /var/lib/clash/default ]]; then
-    mkdir  /var/lib/clash/default
-    cp -f  /var/lib/clash/Country.mmdb /var/lib/clash/default
-fi
-
-exec clash -d /var/lib/clash/default -f /etc/clash/default.yml
+wait -n
+exit $?
