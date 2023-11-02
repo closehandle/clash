a1bee2e90472c078c8282e5c7d897729fe64c09e
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-11-03 02:41:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 CLASH_VERSION='v1.18.0'
 
 wget -O         clash-linux-amd64-$CLASH_VERSION.gz https://github.com/Dreamacro/clash/releases/download/$CLASH_VERSION/clash-linux-amd64-$CLASH_VERSION.gz
-tar xf          clash-linux-amd64-$CLASH_VERSION.gz && rm -f clash-linux-amd64-$CLASH_VERSION.gz
+gzip -d         clash-linux-amd64-$CLASH_VERSION.gz
 chown root:root clash-linux-amd64-$CLASH_VERSION
 chmod +x        clash-linux-amd64-$CLASH_VERSION
 cp -f           clash-linux-amd64-$CLASH_VERSION clash/clash
