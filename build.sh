f15ff7d23a03d3aebe3e5d3a771ed8310d2753aa
CloseHandle <88403145+closehandle@users.noreply.github.com>
2023-11-03 12:46:45 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,4 +9,11 @@ cp -f           clash-linux-amd64-$CLASH_VERSION clash/clash
 cp -f           clash-linux-amd64-$CLASH_VERSION clash-macvlan/clash
 rm -f           clash-linux-amd64-$CLASH_VERSION
 
+wget -O yacd-gh-pages.zip https://github.com/haishanh/yacd/archive/refs/heads/gh-pages.zip
+unzip   yacd-gh-pages.zip && rm -f yacd-gh-pages.zip
+mv -f   yacd-gh-pages yacd
+cp -fr  yacd clash
+cp -fr  yacd clash-macvlan
+rm -fr  yacd
+
 exit 0
